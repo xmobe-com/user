6e8f443c63736ad0ac8e0251c51e7711f0490ce8
Quang Anh <quanganh@aiti.com.vn>
2018-05-07 16:33:03 +0700

    fix tsconfig

--- lib/user.spec.ts
+++ lib/user.spec.ts
@@ -5,9 +5,14 @@ import { User } from './user';
 
 describe('User Package', () => {
   describe('User Properties', () => {
-    it("Should return 'Quang Anh'", () => {
-        let user = new User('Quang Anh', 'Le', 'quanganh@aiti.com.vn', '+840904123456');
-        expect(user.first_name).to.equal('Quang Anh');
+    it('Should return email \'quanganh@aiti.com.vn\'', () => {
+      let user = new User('Quang Anh', 'Le', 'quanganh@aiti.com.vn', '+840904123456');
+      expect(user.email).to.equal('quanganh@aiti.com.vn');
     });
-  })
+
+    it('Should return Full Name Quang Anh Le', () => {
+      let user = new User('Quang Anh', 'Le', 'quanganh@aiti.com.vn', '+840904123456');
+      expect(user.full_name).to.equal('Quang Anh Le');
+    });
+  });
 });
--- lib/user.ts
+++ lib/user.ts
@@ -1,13 +1,34 @@
-export class User {
-    public first_name: string;
-    public last_name: string;
-    public email: string;
-    public phone: string;
+import { IUser } from './user.interface';
 
-    constructor(first_name: string, last_name: string, email: string, phone: string) {
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.email = email;
-        this.phone = phone;
+export class User implements IUser {
+  public id: string;
+  public first_name: string;
+  public last_name: string;
+  public email: string;
+  public phone: string;
+
+  constructor();
+  constructor(user: IUser);
+  constructor(id: string, first_name: string, last_name: string);
+  constructor(id: string, first_name: string, last_name: string, email: string);
+  constructor(id: string, first_name: string, last_name: string, email: string, phone: string);
+  constructor(iOrKey?: IUser | string, first_name?: string, last_name?: string, email?: string, phone?: string) {
+    if (typeof iOrKey == 'object') {
+      this.id = iOrKey.id;
+      this.first_name = iOrKey.first_name;
+      this.last_name = iOrKey.last_name;
+      this.email = iOrKey.email;
+      this.phone = iOrKey.phone;
+    } else if (typeof iOrKey == 'string') {
+      this.id = iOrKey;
+      this.first_name = first_name;
+      this.last_name = last_name;
+      this.email = email;
+      this.phone = phone;
     }
+  }
+
+  get full_name(): string {
+    return this.first_name + ' ' + this.last_name;
+  }
 }
