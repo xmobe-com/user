1a61ce0d2c9460d5eebff8590eadc0c9eeecd69a
Quang Anh <quanganh@aiti.com.vn>
2018-05-08 11:07:34 +0700

    add nyc

--- lib/user.spec.ts
+++ lib/user.spec.ts
@@ -6,12 +6,12 @@ import { User } from './user';
 describe('User Package', () => {
   describe('User Properties', () => {
     it('Should return email \'quanganh@aiti.com.vn\'', () => {
-      let user = new User('Quang Anh', 'Le', 'quanganh@aiti.com.vn', '+840904123456');
+      let user = new User('A0001', 'Quang Anh', 'Le', 'quanganh@aiti.com.vn', '+840904123456');
       expect(user.email).to.equal('quanganh@aiti.com.vn');
     });
 
     it('Should return Full Name Quang Anh Le', () => {
-      let user = new User('Quang Anh', 'Le', 'quanganh@aiti.com.vn', '+840904123456');
+      let user = new User('A0001', 'Quang Anh', 'Le', 'quanganh@aiti.com.vn', '+840904123456');
       expect(user.full_name).to.equal('Quang Anh Le');
     });
   });
